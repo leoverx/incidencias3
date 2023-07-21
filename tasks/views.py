1bf3743ffc8edd3ff177cc213423038d18718ef6
Leonardo Vergara <leonardovergaramarin@gmail.com>
2023-07-21 06:12:29 +0000

    se añade home se mejora signup con form

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def helloworld(request):
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
     return render(request, 'signup.html', {
         'form': UserCreationForm
     })
